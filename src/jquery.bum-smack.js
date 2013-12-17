522eb5a3393a7edee95ec6a573c41553f5e9e913
Sir.Nathan <jstassen@jstassen.com>
2013-12-17 14:07:48 -0600

    Added support for px from bottom of element
    Added support for loading when within a px value from bottom of an
    element or window. Valuable for consistent loading point. Also cleaned
    up variable hoisting, updated readme with new support.

--- src/jquery.bum-smack.js
+++ src/jquery.bum-smack.js
@@ -2,25 +2,27 @@
 
 ;(function ($) {
   'use strict';
+  var $this, deferred, threshold, scrollTop, innerHeight, scrollHeight, distanceFromTop, thresholdFromTop;
 
   // Options:
-  //  threshold: distance (percentage distance from the bottom of the element)
+  //  threshold: distance (percentage or px distance from the bottom of the element)
   $.fn.smack = function (options) {
     options = options || {};
 
-    var $this     = $(this),
+        $this     = $(this),
         deferred  = new $.Deferred(),
         threshold = options.threshold || 1;
 
     $this.on('scroll.smack', function () {
-      var scrollTop     = $this.scrollTop(),
+          scrollTop     = $this.scrollTop();
           // Keep these computed here in case the div changes dimensions at runtime
           innerHeight   = $this[0] === window ? $this.height() : $this.innerHeight(),
           // ScrollHeight doesn't exist on the document nor window
           scrollHeight  = $this[0] === window ? $(document).height() : $this[0].scrollHeight,
 
           distanceFromTop  = scrollTop + innerHeight,
-          thresholdFromTop = scrollHeight * threshold;
+          // Threshold for either percentage and px from bottom
+          thresholdFromTop = (threshold.toString().toLowerCase().indexOf('px') >= 0) ? scrollHeight - parseInt(threshold) : scrollHeight * threshold;
 
       if(distanceFromTop >= thresholdFromTop) {
         $this.off('scroll.smack');
