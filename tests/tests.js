215965a41444c0a84b791494b62bf662e25321bc
Joel Kemp <mrjoelkemp@gmail.com>
2013-12-23 23:40:30 -0500

    Fully tested

--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,42 @@
-var phantom = require('phantom'),
-    path = require('path');
+/* global describe: true, it: true */
+describe('Bum smack', function () {
 
-phantom.create(function(ph) {
-  return ph.createPage(function(page) {
-    return page.open('file://localhost' + path.dirname(__dirname) + '/test.html', function(status) {
-      console.log('opened?', status);
+  it('detect a smack when you reach 100% of the bottom (default)', function (done) {
+    $('.case1').smack().done(done);
+    $('.case1').scrollTop($('.case1')[0].scrollHeight);
 
-      return page.evaluate(function() {
-        return document.title;
-      }, function(result) {
-        console.log('Page title is ' + result);
-        return ph.exit();
-      });
-    });
+    $('.case2').smack().done(done);
+    $('.case2').scrollTop($('.case2')[0].scrollHeight);
   });
+
+  it('detects a percentage threshold smack', function (done) {
+    var $case3 = $('.case3'),
+        scrollHeight = $case3[0].scrollHeight,
+        innerHeight = $case3.innerHeight(),
+        offset = scrollHeight - innerHeight,
+        eightyPercent = offset * 0.8;
+
+    $('.case3').smack({ threshold: 0.8 }).done(done);
+    $('.case3').scrollTop(eightyPercent);
+  });
+
+  it('detects a pixel distance smack', function (done) {
+    var $case4 = $('.case4'),
+        scrollHeight = $case4[0].scrollHeight,
+        innerHeight = $case4.innerHeight(),
+        offset = scrollHeight - innerHeight,
+        fiftyPixelsAway = offset - 50;
+
+    $('.case4').smack({ threshold: '50px' }).done(done);
+    $('.case4').scrollTop(fiftyPixelsAway);
+  });
+
+  it('detects smacking the bottom of the page', function (done) {
+    $(window).smack().done(function () {
+      done();
+      $(window).scrollTop(0);
+    });
+    $(window).scrollTop($(document).height());
+  });
+
 });
