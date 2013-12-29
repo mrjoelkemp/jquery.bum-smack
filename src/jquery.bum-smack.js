15e6523306bb92ef770b6a28cfd5a49b276c0060
Joel Kemp <mrjoelkemp@gmail.com>
2013-12-29 11:17:24 -0500

    Comments for clarification. Moved the setting of data attribute to only when the smack options change

--- src/jquery.bum-smack.js
+++ src/jquery.bum-smack.js
@@ -5,18 +5,28 @@
   //  threshold: distance (percentage or px distance from the bottom of the element)
   //  edge: 'top' or 'bottom' (which edge of the element to watch)
   $.fn.smack = function (options) {
+    // Default edge is bottom
     options = $.extend({}, {'edge': 'bottom'}, options);
+    // Default is the absolute top of the screen or bottom of the screen
+    // Override with any newly supplied options
     options = $.extend({}, {
         'threshold' : options.edge === 'top' ? 0 : 1,
         'deferred'  : new $.Deferred()
     }, options);
 
     var $this           = $(this),
+        // If this element has been bound before, pull up its last options
+        // Note: Since we only allow for a single smack binding,
+        //    this means that if you had a previous (unsmacked) binding for 'top'
+        //    and are now binding for the 'bottom', we'll still remember your
+        //    'top' binding by pulling it from the data attribute on the bind for 'bottom'.
         bumSmackOptions = $this.data('bum-smack') || {};
 
+    // Use the new options to replace the previous one
     bumSmackOptions[options.edge] = options;
     $this.data('bum-smack', bumSmackOptions);
 
+    // Only allow a single scroll event binding
     $this.off('scroll.smack').on('scroll.smack', function () {
       var scrollTop     = $this.scrollTop(),
           // Keep these computed here in case the div changes dimensions at runtime
@@ -36,6 +46,7 @@
         if (distanceFromTop >= thresholdFromBottom) {
             bumSmackOptions.bottom.deferred.resolve();
             bumSmackOptions.bottom = false;
+            $this.data('bum-smack', bumSmackOptions);
         }
       }
 
@@ -47,11 +58,11 @@
         if (scrollTop <= thresholdFromTop) {
             bumSmackOptions.top.deferred.resolve();
             bumSmackOptions.top = false;
+            $this.data('bum-smack', bumSmackOptions);
         }
       }
 
-      $this.data('bum-smack', bumSmackOptions);
-
+      // If the configured smack for the edge has been met
       if (!bumSmackOptions.top && !bumSmackOptions.bottom) {
         $this.off('scroll.smack');
       }
