94284a3ed53b65721150dd35e9e452db2fefff7f
Joel Kemp <mrjoelkemp@gmail.com>
2014-02-24 22:48:39 -0500

    Additional helpers to clean up the tests

--- test/tests.js
+++ test/tests.js
@@ -25,24 +25,42 @@ describe('Bum smack', function () {
         return result;
       };
 
+  $.fn.toBottom = function () {
+    this.scrollTop(getPercentageScrollTop(this, 1));
+    return this;
+  };
+  $.fn.toTop = function () {
+    this.scrollTop(0);
+    return this;
+  };
+  $.fn.toPercentFromTop = function (percent) {
+    if (percent > 1) percent = percent / 100;
+    this.scrollTop(getPercentageScrollTop(this, percent));
+    return this;
+  };
+  $.fn.toPixelsFromTop = function (pixel) {
+    this.scrollTop(getPixelScrollTop(this, parseInt(pixel, 10)));
+    return this;
+  };
+
   it('detects a bottom smack when you reach 100% of the bottom (default)', function (done) {
     $('.case1').smack().done(done);
-    $('.case1').scrollTop($('.case1')[0].scrollHeight);
+    $('.case1').toBottom();
 
     $('.case2').smack({ edge: 'bottom' }).done(done);
-    $('.case2').scrollTop($('.case2')[0].scrollHeight);
+    $('.case2').toBottom();
   });
 
   it('detects a top smack when you reach 100% of the top when the edge is set', function (done) {
-    // Case 1 is at the bottom from the previous test
+    $('.case1').toBottom();
     $('.case1').smack({ edge: 'top' }).done(done);
-    $('.case1').scrollTop(0);
+    $('.case1').toTop();
   });
 
   it('does not smack the top edge if the element was already at the top', function (done) {
     var cb = sinon.spy();
 
-    $('.case1').scrollTop(0);
+    $('.case1').toTop();
     $('.case1').smack({ edge: 'top' }).done(cb);
     $('.case1').scrollTop(10);
     $('.case1').smack({ edge: 'top' }).done(function () {
@@ -50,20 +68,20 @@ describe('Bum smack', function () {
       done();
     });
 
-    $('.case1').scrollTop(0);
+    $('.case1').toTop();
   });
 
   it('only has one active smack binding', function (done) {
     var cb1 = sinon.spy();
 
-    $('.case1').scrollTop(0);
+    $('.case1').toTop();
     $('.case1').on('scroll.smack', cb1);
     $('.case1').smack({ threshold: 0.8 }).done(function () {
       chai.expect(cb1.called).to.equal(false);
       done();
     });
 
-    $('.case1').scrollTop(getPercentageScrollTop($('.case1'), 0.8));
+    $('.case1').toPercentFromTop(0.8);
   });
 
   // Bind to the bottom edge
@@ -71,49 +89,53 @@ describe('Bum smack', function () {
   // Smack the bottom (expect done)
   // Smack the top (expect done)
   it('allows for binding to both edges with different promise callbacks', function (done) {
-    $('.case1').scrollTop(0);
+    $('.case1').toTop();
 
     $('.case1').smack({ threshold: 0.8 }).done(function () {
       // Smack top
-      $('.case1').scrollTop(0);
+      $('.case1').toTop();
     });
 
     $('.case1').smack({ edge: 'top' }).done(function () {
       done();
     });
 
-    $('.case1').scrollTop(getPercentageScrollTop($('.case1'), 0.8));
+    $('.case1').toPercentFromTop(0.8);
   });
 
 
   it('smacks a percentage threshold away from the bottom', function (done) {
     $('.case3').smack({ threshold: 0.8 }).done(done);
-    $('.case3').scrollTop(getPercentageScrollTop($('.case3'), 0.8));
+    $('.case3').toPercentFromTop(0.8);
   });
 
-  it.skip('smacks a percentage threshold away from the top', function (done) {
-    // Reset case 3 to the bottom
-    $('.case3').scrollTop($('.case3')[0].scrollHeight);
+  it('smacks a percentage threshold away from the top', function (done) {
+    $('.case3').toBottom();
 
     $('.case3').smack({ edge: 'top', threshold: 0.2 }).done(done);
-    $('.case3').scrollTop(getPercentageScrollTop($('.case3'), 0.2));
+    $('.case3').toPercentFromTop(0.2);
+  });
+
+  it('should not smack the top threshold scrolling down', function () {
+
   });
 
   it('smacks a pixel distance away from the bottom', function (done) {
     $('.case4').smack({ threshold: '50px' }).done(done);
-    $('.case4').scrollTop(getPixelScrollTop($('.case4'), 50));
+    $('.case4').toPixelsFromTop(50);
   });
 
-  it.skip('smacks a pixel distance away from the top', function (done) {
-    $('.case4').scrollTop(getPixelScrollTop($('.case4'), 100));
+  it('smacks a pixel distance away from the top', function (done) {
+    $('.case4').toTop();
+    $('.case4').toPixelsFromTop(100);
     $('.case4').smack({ edge: 'top', threshold: '50px' }).done(done);
-    $('.case4').scrollTop(getPixelScrollTop($('.case4'), 50));
+    $('.case4').toPixelsFromTop(50);
   });
 
   it('smacks the bottom of the page', function (done) {
     $(window).smack().done(function () {
       done();
-      $(window).scrollTop(0);
+      $(window).toTop();
     });
     $(window).scrollTop($(document).height());
   });
